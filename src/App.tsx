f8c69290a8b9349d8185f0efa991c449ddb70680
R3laxation <memories@bk.ru>
2022-09-18 17:26:32 +0600

    Started Lesson0 component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Lesson1 from './lessons/lesson1/lesson1';
 import Lesson3 from './lessons/lesson3/Lesson3';
 import Lesson4 from './lessons/lesson4/Lesson4';
+import {Lesson0} from "./lessons/lesson0/Lesson0";
 // import Lesson1 from './lessons/lesson1/lesson1';
 // import './lessons/lesson2/lesson2';
 // import Lesson3 from './lessons/lesson3/Lesson3';
@@ -15,6 +16,7 @@ import Lesson4 from './lessons/lesson4/Lesson4';
 function App() {
     return (
         <div className="container">
+            <Lesson0/>
             {/*<Lesson1 />*/}
             {/*<Lesson3 />*/}
             {/*<Lesson4 />*/}
---
+++ src/lessons/lesson0/Lesson0.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Lesson0 = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
