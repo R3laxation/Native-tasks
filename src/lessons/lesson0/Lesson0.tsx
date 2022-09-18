564d6b3d813a7603f4cc81df29d647061e4e3890
R3laxation <memories@bk.ru>
2022-09-18 20:46:21 +0600

    Added Sum of array Task № 1

--- src/lessons/lesson0/Lesson0.tsx
+++ src/lessons/lesson0/Lesson0.tsx
@@ -3,9 +3,10 @@ import React from 'react';
 export const Lesson0 = () => {
     return (
         <div>
+            {/*1. Summa*/}
+            {/*const array = [1, 2, 3, 4, 5]*/}
 
-
-
+            {/*eval(array.join('+'))*/}
 
 
 
