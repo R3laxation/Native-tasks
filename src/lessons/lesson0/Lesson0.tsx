31466e58207bbdb3ba15d0adafd7715105a47a6d
R3laxation <memories@bk.ru>
2022-09-18 20:53:52 +0600

    Added Task № 2, Max element of array

--- src/lessons/lesson0/Lesson0.tsx
+++ src/lessons/lesson0/Lesson0.tsx
@@ -3,13 +3,6 @@ import React from 'react';
 export const Lesson0 = () => {
     return (
         <div>
-            {/*1. Summa*/}
-            {/*const array = [1, 2, 3, 4, 5]*/}
-
-            {/*eval(array.join('+'))*/}
-
-
-
 
 
 
---
+++ src/lessons/lesson0/Tasks.js
@@ -0,0 +1,15 @@
+{/*1. Summa*/}
+{/*const array = [1, 2, 3, 4, 5]*/}
+
+{/*eval(array.join('+'))*/}
+
+
+{/*2. Max*/}
+// max = arr[i]
+// for(let i = 1; i < arr.length; i++) {
+//     max = arr[i] > max ? arr[i] : max;
+//         ili
+
+// Math.max(arr[i], max)
+
+// }
