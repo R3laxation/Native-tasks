c31817f62ec7af36a4ed46c54509eb620b25c8d2
R3laxation <memories@bk.ru>
2022-09-29 23:24:01 +0600

    Task № 33. Simple search. Wrote second decision

--- src/lessons/lesson0/Tasks.js
+++ src/lessons/lesson0/Tasks.js
@@ -656,14 +656,46 @@ counter2()      1
 **Output**: Number
 
 
+// function search(array, target) {
+//     for(let i = 0; i<array.length; i++){
+//         if(array[i] === target){
+//             return i
+//         }
+//
+//     }
+//     return -1
+// }
+
 function search(array, target) {
-    for(let i = 0; i<array.length; i++){
-        if(array[i] === target){
-            return i
+    let start = 0;
+    let end = array.length-1;
+    if(target < array[start] || target > array[end]){
+        return -1
+    }
+
+    while (true){
+        if(target === array[start]){
+            return start
+        }
+        if(target === array[end]){
+            return end
+        }
+        if(end - start <= 1){
+            return -1
+        }
+
+
+        const middle = Math.floor((start + end) / 2);
+
+        if (target > array[middle]){
+            start = middle + 1
+        } else if(target < array[middle]){
+            end = middle -1
+        } else {
+            return middle
         }
 
     }
-    return -1
 }
 
 console.log(search([1, 3, 6, 13, 17], 13)) // -> 3
