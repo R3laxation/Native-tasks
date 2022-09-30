57a3b676a803b1c45983375d17c03518a51f8dd2
R3laxation <memories@bk.ru>
2022-09-30 20:19:43 +0600

    Commented. Finished the day

--- src/lessons/lesson0/Tasks.js
+++ src/lessons/lesson0/Tasks.js
@@ -700,41 +700,41 @@ counter2()      1
 // console.log(search([1, 3, 6, 13, 17], 13)) // -> 3
 // console.log(search([1, 3, 6, 13, 17], 12)) // -> -1
 
- 34. Balanced brackets
-
-Напишите функцию, которая проверит строку на сбалансированность скобок: `{}, (), []`. Вернуть `true` если они сбалансированы, иначе `false`.
-
-**Input**: String
-
-**Output**: Boolean
-
-function isBalanced(string) {
-   const start  = '{[(';
-   const end = '}])';
-   const queue = [];
-   const map = {
-       '}':'{',
-       ']':'[',
-       ')':'('
-   }
-
-   for (let i = 0; i<string.length; i++){
-       const char = string[i];
-
-       if(start.includes(char)){
-            queue.push(char)
-       } else if (end.includes(char)){
-          const last =  queue.pop()
-           if(map[char] !==last){
-               return false
-           }
-       }
-   }
-   return !queue.length
-}
-
-console.log(isBalanced('(x + y) - (4)')) // -> true
-console.log(isBalanced('(((10 ) ()) ((?)(:)))')) // -> true
-console.log(isBalanced('[{()}]')) // -> true
-console.log(isBalanced('(50)(')) // -> false
-console.log(isBalanced('[{]}')) // -> false
+//  34. Balanced brackets
+//
+// Напишите функцию, которая проверит строку на сбалансированность скобок: `{}, (), []`. Вернуть `true` если они сбалансированы, иначе `false`.
+//
+// **Input**: String
+//
+// **Output**: Boolean
+//
+// function isBalanced(string) {
+//    const start  = '{[(';
+//    const end = '}])';
+//    const queue = [];
+//    const map = {
+//        '}':'{',
+//        ']':'[',
+//        ')':'('
+//    }
+//
+//    for (let i = 0; i<string.length; i++){
+//        const char = string[i];
+//
+//        if(start.includes(char)){
+//             queue.push(char)
+//        } else if (end.includes(char)){
+//           const last =  queue.pop()
+//            if(map[char] !==last){
+//                return false
+//            }
+//        }
+//    }
+//    return !queue.length
+// }
+//
+// console.log(isBalanced('(x + y) - (4)')) // -> true
+// console.log(isBalanced('(((10 ) ()) ((?)(:)))')) // -> true
+// console.log(isBalanced('[{()}]')) // -> true
+// console.log(isBalanced('(50)(')) // -> false
+// console.log(isBalanced('[{]}')) // -> false
