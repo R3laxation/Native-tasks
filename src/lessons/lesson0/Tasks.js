e4dbfa951288b179adf3c8982062165e262951d4
R3laxation <memories@bk.ru>
2022-10-03 20:04:11 +0600

    Task № 37 - Fibonacci. Wrote iife function skeleton

--- src/lessons/lesson0/Tasks.js
+++ src/lessons/lesson0/Tasks.js
@@ -872,31 +872,36 @@ counter2()      1
 // console.log(deepEqual(NaN, NaN)) // -> true
 // console.log(deepEqual('test', 'test!')) // -> false
 // console.log(deepEqual()) // -> true
-37. Fibonacci
+// 37. Fibonacci
+//
+// Напишите функцию, которая будет генерировать последовательность Фиббоначи длинны `n`, которую передали как аргумент.
+//
+// **Input**: Number
+//
+// **Output**: Number[]
+//
+// function fibonacci(n) {
+//     const seq = [1, 1]
+//
+//     if(n < 2){
+//         return seq.slice(0, n)
+//     }
+//
+//     while(seq.length < n){
+//
+//         const last = seq[seq.length -1]
+//         const prev = seq[seq.length -2]
+//
+//         seq.push(last+prev)
+//     }
+//
+//     return seq
+// }
 
-Напишите функцию, которая будет генерировать последовательность Фиббоначи длинны `n`, которую передали как аргумент.
+const fibonacci = function (function(){
 
-**Input**: Number
+})()
 
-**Output**: Number[]
-
-function fibonacci(n) {
-    const seq = [1, 1]
-
-    if(n < 2){
-        return seq.slice(0, n)
-    }
-
-    while(seq.length < n){
-
-        const last = seq[seq.length -1]
-        const prev = seq[seq.length -2]
-
-        seq.push(last+prev)
-    }
-
-    return seq
-}
 
 console.log(fibonacci(8))
 console.log(fibonacci(15))
