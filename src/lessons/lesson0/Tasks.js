606ca38dba86b2cdcba211c961d6c7726f07143f
R3laxation <memories@bk.ru>
2022-10-10 21:11:02 +0600

    Task № 41. FizzBuz. Wrote first if

--- src/lessons/lesson0/Tasks.js
+++ src/lessons/lesson0/Tasks.js
@@ -1009,6 +1009,8 @@ counter2()      1
 
 const fizzBuzz = num => {
     for(let i = 0; i<=num; i++){
-
+        if(i % 3 === 0 && i % 5 === 0) {
+            console.log('fizzbuzz')
+        }
     }
 }
