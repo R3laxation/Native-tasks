f354cdc2db828071acad87ee9154391c31cfb316
R3laxation <memories@bk.ru>
2022-10-11 18:36:34 +0600

    Task № 42. Find vowels. Completed first realization

--- src/lessons/lesson0/Tasks.js
+++ src/lessons/lesson0/Tasks.js
@@ -1032,6 +1032,9 @@ const findVowels = str => {
     let count = 0;
     const vowels = ['a', 'e', 'i', 'o', 'u'];
     for(let char of str.toLowerCase()){
-
+        if(vowels.includes(char)){
+            count++
+        }
     }
+    return count
 }
