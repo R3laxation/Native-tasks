e640963b2ee4199440c8fc756453c1f681f6a133
R3laxation <memories@bk.ru>
2022-10-19 21:20:59 +0600

    Task № 46. Function log. Update function

--- src/lessons/lesson0/Tasks.js
+++ src/lessons/lesson0/Tasks.js
@@ -1136,4 +1136,6 @@ tenNumbersModulator(10, 100); // 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
 
 window.configuration.isEntry = true;
 
+fiveNumbersModulator(configuration.start, configuration.end); // 46, 51, 56, 61, 66
+
 
